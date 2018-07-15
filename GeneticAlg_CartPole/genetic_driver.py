d3770475911ee0f3ce6f84ee8cdbb80e2e3ff69a
Cameron White <cameronwhite08@gmail.com>
2018-07-14 23:59:46 -0400

    neural net now evolves to play cartpole

--- GeneticAlg_CartPole/genetic_driver.py
+++ GeneticAlg_CartPole/genetic_driver.py
@@ -1,34 +1,57 @@
 import random
 import time
 
-import neural_net as net;
+import neural_net as net
 
-from deap import base
-from deap import creator
-from deap import tools
+from deap import base, creator, tools, algorithms
+import multiprocessing
 
 import numpy as np
 
+import gym
+
 # global control vars
 population_size = 50
-Mutation_Probability = 0.8
-current_max_fitness = 1
-generation_counter = 0
-gens_before_print = 100
-pop_keep_percent = .2
+num_generations = 5
+mutation_probability = 0.2
+mate_probability = .5
+current_env = 'CartPole-v0'
 
-num_inputs = 4
-num_outputs = 2
+net_inputs = 4
+net_outputs = 2
 # only 1 hidden layer for now
 num_hidden_nodes = 5
 
 
-# this is going to change considerably
-def mutate_individual(individual, indpb=.5):
+def mutate_individual(individual, indpb=.1):
     for idx in range(len(individual)):
-        if random.random() < indpb:
-            individual[idx] = rand_weight()
-    return individual
+        # randomly mutate individual genes
+        if np.random.random(1) < indpb:
+            # randomly decide the mutation
+            decision = float(np.random.random(1))
+
+            rand = float(np.random.random(1))
+
+            # whole new weight
+            if decision < .25:
+                individual[idx] = 2 * rand - 1
+                continue
+            # change weight by random percentage
+            if decision < .5:
+                multiplier = rand / 1.5  # get the multiplier on the range of [.5,1.5)
+                individual[idx] = individual[idx] * multiplier
+                continue
+            if decision < .75:
+                sign = 1
+                # to make this randomly addition or subtraction
+                if np.random.random(1) < .5:
+                    sign = -sign
+                individual[idx] += (rand * sign)
+                continue
+            # do something as if 1>rand>.75
+            individual[idx] = -individual[idx]
+
+    return individual,
 
 
 def ind_to_np_array(x):
@@ -38,7 +61,7 @@ def ind_to_np_array(x):
 def create_individual(individual):
     weights = []
     # 4 inputs, 2 outputs, 5 nodes in hidden layer
-    layer1_weights, layer2_weights = net.get_rand_weights(num_inputs, num_outputs, num_hidden_nodes)
+    layer1_weights, layer2_weights = net.get_rand_weights(net_inputs, net_outputs, num_hidden_nodes)
 
     # flatten weight arrays so it can be stored in the individual
     flat1 = layer1_weights.flatten()
@@ -51,39 +74,46 @@ def create_individual(individual):
     return individual(x for x in weights)
 
 
-# the goal ('fitness') function to be maximized
 # this will be a loop for a individual's try at the game
-def evaluate_individual(individual):
+def evaluate_individual(individual, display=False):
+    flat_weights = ind_to_np_array(individual)
+    # find split point of flat_weights
+    num_first_layer_weights = net_inputs * num_hidden_nodes
 
-    arr = ind_to_np_array(individual)
+    layer1_w = np.reshape(flat_weights[:num_first_layer_weights], (net_inputs, num_hidden_nodes))
+    layer2_w = np.reshape(flat_weights[num_first_layer_weights:], (num_hidden_nodes, net_outputs))
 
-    num_first_layer_weights = num_inputs * num_hidden_nodes
+    # do this to avoid 200 step limit
+    env = gym.make(current_env).env
+    observation = env.reset()
+    fitness = 0
 
-    layer1_w = np.reshape(arr[:num_first_layer_weights], (num_inputs, num_hidden_nodes))
-    layer2_w = np.reshape(arr[num_first_layer_weights:], (num_hidden_nodes, num_outputs))
+    for t in range(1500):
+        if display:
+            env.render()
+        # print(observation)
 
-    l1 = forward_prop(individual)
+        # get observation into np array
+        obs = ind_to_np_array(observation)
 
-    # how much did we miss?
-    l1_error = y - l1
-    # print('eval: ' + str(l1) + '\t' + str(abs(1-np.sum(l1_error))))
-    # print('eval2: ' + str(l1) + '\t' + str(abs(np.sum(l1_error))))
-    # print()
-    return float(np.sum(np.power(l1_error, 2))),
+        # forward propagate based on the observation and store result as a list
+        result = list(net.forward_prop(obs, layer1_w, layer2_w))
 
+        # action agent will take is based on output with max result
+        max_index = result.index(max(result))
 
-def print_gen_info(print_divider=True):
-    print("Generation {0} outputs: {1} -- fitness: {2}".format(generation_counter,
-                                                               forward_prop(best_individual),
-                                                               current_max_fitness))
-    print("Evolved weights {0}:{1}".format(best_individual[0], best_individual[1]))
-    if print_divider:
-        print('-' * 20)
+        # take a step
+        observation, reward, done, info = env.step(max_index)
 
+        # add reward to agent's fitness
+        fitness += reward
 
-# seed the random
-random.seed(int(time.time()))
-np.random.seed(int(time.time()))
+        if done:
+            if display:
+                print("Episode finished after {} time steps".format(t + 1))
+            break
+    return fitness,
+
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
@@ -97,74 +127,53 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 # register the fitness function
 toolbox.register("evaluate", evaluate_individual)
 # register the crossover function
-toolbox.register("crossover", tools.cxUniform, indpb=.5)
+toolbox.register("mate", tools.cxTwoPoint)
 # register a mutation operator with a probability to flip each gene of 0.05
 toolbox.register("mutate", mutate_individual)
 # set the selection method to grab the top performers
-toolbox.register("select", tools.selBest)
+toolbox.register("select", tools.selTournament, tournsize=3)
 
+if __name__ == '__main__':
+    # seed the random
+    random.seed(int(time.time()))
+    np.random.seed(int(time.time()))
 
-# create an initial population of individuals
-pop = toolbox.population(n=population_size)
+    # Process Pool of 8 workers
+    pool = multiprocessing.Pool(processes=8)
+    toolbox.register("map", pool.map)
 
-# Evaluate the entire population
-fitnesses = list(map(toolbox.evaluate, pop))
-for ind, fit in zip(pop, fitnesses):
-    ind.fitness.values = fit
+    try:
+        # create an initial population of individuals
+        pop = toolbox.population(n=population_size)
+        hof = tools.HallOfFame(1)
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean)
+        stats.register("std", np.std)
+        stats.register("min", np.min)
+        stats.register("max", np.max)
 
-# Evolution loop
-while abs(current_max_fitness) > .01:
+        current_max_fitness = 0
 
-    # Select the top % of the population
-    top_performers = toolbox.select(pop, int(len(pop) * pop_keep_percent))  # take % of population
+        # Evolution loop
+        while current_max_fitness < 1000:
+            pop, book = algorithms.eaSimple(pop, toolbox,
+                                            cxpb=mate_probability,  # probability of mating 2 individuals
+                                            mutpb=mutation_probability,  # probability of mutating an individual
+                                            ngen=num_generations,  # number of generations
+                                            stats=stats,  # statistics object
+                                            halloffame=hof)  # contains the best individuals
 
-    # Randomly shuffle the population before crossover
-    random.shuffle(top_performers)
+            bestInd = hof
 
-    next_gen = []
+            current_fitness = float(bestInd[0].fitness.values[0])
+            if current_fitness > current_max_fitness:
+                current_max_fitness = current_fitness
+                print("Current max fitness: {0}".format(current_max_fitness))
+                evaluate_individual(bestInd[0], True)
 
-    # Produce enough offspring until the population is refilled
-    # loop until we refill population
-    while len(next_gen) < len(pop):
-        # to get random top performers
-        i = np.random.randint(0, len(top_performers))
-        j = np.random.randint(0, len(top_performers))
+            print("Current max fitness: {0}".format(current_max_fitness))
+    # always close the thread pool
+    finally:
+        pool.close()
 
-        # don't want to crossover with oneself
-        if i == j:
-            continue
-
-        # clone new children to crossover
-        child1 = toolbox.clone(top_performers[i])
-        child2 = toolbox.clone(top_performers[j])
-
-        # crossover
-        new_children = toolbox.crossover(child1, child2)
-
-        # save into next generation
-        next_gen.extend(new_children)
-
-    for mutant in next_gen:
-        # mutate an individual with probability Mutation_Probability
-        if random.random() < Mutation_Probability:
-            toolbox.mutate(mutant)
-
-    # Evaluate the individuals
-    population = [ind_to_np_array(x) for x in next_gen]
-    fitnesses = map(toolbox.evaluate, population)
-    for ind, fit in zip(next_gen, fitnesses):
-        ind.fitness.values = fit
-
-    # The population is entirely replaced by the offspring
-    pop[:] = next_gen
-
-    best_individual = tools.selBest(pop, 1)
-    best_individual = best_individual[0]
-    current_max_fitness = best_individual.fitness.values[0]
-
-    if generation_counter % gens_before_print is 0:
-        print_gen_info()
-    generation_counter = generation_counter + 1
-
-print("-- End of evolution --")
-print_gen_info(print_divider=False)
+    print("-- End of evolution --")
